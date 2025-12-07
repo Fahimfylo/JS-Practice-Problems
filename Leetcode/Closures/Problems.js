db373ca59cd108fc7d494a0ab480a60ab2f39c07
ghost <ghostpy91.com>
2025-12-08 04:30:00 +0600

    closures

--- Leetcode/Closures/Problems.js
+++ Leetcode/Closures/Problems.js
@@ -79,19 +79,3 @@ let createCounter2 = function (init) {
 // const counter = createCounter2(5);
 // console.log(counter.increment());
 
-
-// 2635 - Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element. The returned array should be created such that returnedArray[i] = fn(arr[i], i).
-let map = function(arr, fn) {
-    let newArr = [];
-    arr.forEach((value, index) => {
-        newArr.push(fn(value, index));
-    });
-    return newArr;
-};
-
-let arr = [1, 2, 3];
-
-function multiplyByIndex(value, index) {
-    return value * index;
-}
-console.log(map(arr, multiplyByIndex));
