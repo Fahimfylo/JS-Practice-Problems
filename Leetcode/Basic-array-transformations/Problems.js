528338655bc871398d45d67483986fdf347a4e30
ghost <ghostpy91.com>
2025-12-14 17:02:16 +0600

    Array Transformations

--- Leetcode/Basic-array-transformations/Problems.js
+++ Leetcode/Basic-array-transformations/Problems.js
@@ -28,7 +28,7 @@ let filter = function (arr, fn) {
 // 2626 - Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
 // This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
 // If the length of the array is 0, the function should return init.
-// Please solve it without using the built-in Array.reduce method.
+// Please solve it without using the built-in Array.reduce() method
 var reduce = function (nums, fn, init) {
   let val = init;
   if (nums.length === 0) {
